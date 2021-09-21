7497bf9086d1850de16d3c564fa6ed7767152919
astalon <erik@lagerstrom.com>
2021-09-21 21:37:46 +0200

    Moved initial java projects from local machine to repo

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Erik, you rock!");
+
+        int myNumber = 2;
+        int mySecondNumber = 5;
+        int myThirdNumber = myNumber + mySecondNumber;
+        System.out.println(myThirdNumber);
+    }
+}
