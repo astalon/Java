3905bb713d8d3f428aadd3523eb4dd946b93aeb5
astalon <erik@lagerstrom.com>
2021-09-21 23:18:40 +0200

    Further notes, small coding example

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        byte myByte = 10;
+        short theShort = 28;
+        int mrInt = 216;
+        short coolShort = (short) (1000 + 10*(myByte + theShort + mrInt));
+        System.out.println(coolShort);
 
     }
 }
